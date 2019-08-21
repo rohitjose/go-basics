807a1f13c80f35c82a4a5ae0fa6b9f6d4b2b4095
Rohit Jose <rohitjose@gmail.com>
2019-08-22 04:40:46 +1000

    Further updates

---
+++ loop/main.go
@@ -0,0 +1,42 @@
+// In Go, _variables_ are explicitly declared and used by
+// the compiler to e.g. check type-correctness of function
+// calls.
+
+package main
+
+import "fmt"
+
+func main() {
+	// start run OMIT
+	// The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	// A classic initial/condition/after `for` loop.
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+	// end run OMIT
+	// start run2 OMIT
+	// `for` without a condition will loop repeatedly
+	// until you `break` out of the loop or `return` from
+	// the enclosing function.
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	// You can also `continue` to the next iteration of
+	// the loop.
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+	// end run2 OMIT
+
+}

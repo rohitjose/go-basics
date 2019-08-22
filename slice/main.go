8c605e22f4df1b465ae7ed60417b3f03203da68f
rjose2 <Rohit.Jose@macquarie.com>
2019-08-22 13:26:17 +1000

    Changes for slice

--- slice/main.go
+++ slice/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	numbers = append(numbers, 100, 200,300)
+	numbers = numbers[2:]
+	numbers = numbers[:11]
 
 	for _, number := range numbers {
 		if number%2 == 0 {
